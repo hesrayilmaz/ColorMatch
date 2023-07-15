c96f763b33f7dd1441790bf873d542c9e74dec67
hesrayilmaz <hesra.yilmazz@gmail.com>
2023-07-16 00:05:17 +0300

    add background music

---
+++ Assets/Scripts/BackgroundMusicController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+	public static BackgroundMusicController instance;
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
